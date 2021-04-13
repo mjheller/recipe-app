661ce698ac3235b5878a7e77f957e4dda263157f
mjheller <mjheller414@gmail.com>
2021-04-13 14:35:26 -0500

    updated to angular 11

--- src/app/components/recipe-create/recipe-create.component.spec.ts
+++ src/app/components/recipe-create/recipe-create.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RecipeCreateComponent } from './recipe-create.component';
 
@@ -6,7 +6,7 @@ describe('RecipeCreateComponent', () => {
   let component: RecipeCreateComponent;
   let fixture: ComponentFixture<RecipeCreateComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RecipeCreateComponent ]
     })
